59f79757ffbf12ca12bdb74b8221061f72feb37f
martinmihov <martinmihovofficial@gmail.com>
2017-04-19 15:37:56 +0300

    -----Spring Security: --Edit UserController.java --Add src/main/resources/templates/user/login.html --Add src/main/resources/templates/user/profile.html

--- src/main/java/martinBlog/controller/UserController.java
+++ src/main/java/martinBlog/controller/UserController.java
@@ -6,11 +6,21 @@ import martinBlog.entity.User;
 import martinBlog.repository.RoleRepository;
 import martinBlog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Martin on 4/18/2017.
@@ -51,5 +61,39 @@ public class UserController {
         this.userRepository.saveAndFlush(user);
 
         return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("view", "user/login");
+
+        return "base-layout";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login?logout";
+    }
+
+    @GetMapping("/profile")
+    @PreAuthorize("isAuthenticated()")
+    public String profilePage(Model model){
+        UserDetails principal = (UserDetails) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        User user = this.userRepository.findByEmail(principal.getUsername());
+
+        model.addAttribute("user", user);
+        model.addAttribute("view", "user/profile");
+
+        return "base-layout";
     }
 }
