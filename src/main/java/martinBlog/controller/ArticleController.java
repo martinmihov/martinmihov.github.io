c01385bec0f3c6f5e36deeb005c3a0e0debec4a7
martinmihov <martinmihovofficial@gmail.com>
2017-04-20 09:29:07 +0300

    -----Editing Articles --Edit controller/ArticleController.java --Edit resources/templates/article/details.html --Add resources/templates/article/edit.html

--- src/main/java/martinBlog/controller/ArticleController.java
+++ src/main/java/martinBlog/controller/ArticleController.java
@@ -68,4 +68,37 @@ public class ArticleController {
 
         return "base-layout";
     }
+
+    @GetMapping("/article/edit/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String edit(@PathVariable Integer id, Model model) {
+
+        if (!this.articleRepository.exists(id)){
+            return "redirect:/";
+        }
+
+        Article article = this.articleRepository.findOne(id);
+
+        model.addAttribute("view", "article/edit");
+        model.addAttribute("article", article);
+
+        return "base-layout";
+    }
+
+    @PostMapping("/article/edit/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String editProcess(@PathVariable Integer id, ArticleBindingModel articleBindingModel) {
+        if(!this.articleRepository.exists(id)){
+            return "redirect:/";
+        }
+
+        Article article = this.articleRepository.findOne(id);
+
+        article.setContent(articleBindingModel.getContent());
+        article.setTitle(articleBindingModel.getTitle());
+
+        this.articleRepository.saveAndFlush(article);
+
+        return "redirect:/article/" + article.getId();
+    }
 }
